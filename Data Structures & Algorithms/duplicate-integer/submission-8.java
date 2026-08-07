1e5d1bb6942f2503a75c190442ee11e26efe915a
luislerit <luis_lerit@yahoo.com>
2026-08-07 14:15:49 -0600

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+        Set<Integer> seen = new HashSet<>();
+        for(int n : nums) {
+            if(!seen.contains(n)) {
+                seen.add(n);
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
