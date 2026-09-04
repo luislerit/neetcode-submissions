0333cac4c983ecc92eda2b487ec5bab83e045fad
luislerit <luis_lerit@yahoo.com>
2026-09-03 20:20:09 -0600

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int counter = 1;
+        int L = 0;
+        int R = s.length()-1;
+
+        while(L < R) {
+            if(s.charAt(L) != s.charAt(R)) {
+                return isPalindrome(s, L, R-1) || isPalindrome(s,L+1,R);
+            }
+            else if(s.charAt(L) == s.charAt(R)) {
+                L++;
+                R--;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome(String s, int i, int j){
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
